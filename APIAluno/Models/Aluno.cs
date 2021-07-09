031cf3c2b4348a5ca1e5cd8779cdc105e6583f8c
carl0sR0ma0 <cvpromao@gmail.com>
2021-07-09 15:35:27 -0300

    Add project files.

---
+++ APIAluno/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIAluno.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Dt_Birth { get; set; }
+    }
+}
---
+++ APIAluno/Models/AlunoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIAluno.Models
+{
+    public class AlunoContext : DbContext
+    {
+        public AlunoContext(DbContextOptions<AlunoContext> options) : base(options)
+        { }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
